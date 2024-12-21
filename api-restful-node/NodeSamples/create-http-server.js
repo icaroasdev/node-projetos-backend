747aa7855da5f99e79c91abbca488ee0c5537873
Ícaro Alencar <ias_epf@hotmail.com>
2024-12-20 23:29:35 -0300

    Alteração do Package.json para type: modules e criação de um sample do node

---
+++ api-restful-node/NodeSamples/create-http-server.js
@@ -0,0 +1,11 @@
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+});
+
